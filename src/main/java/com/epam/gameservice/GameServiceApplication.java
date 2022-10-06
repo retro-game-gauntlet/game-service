296be3ceb874e05559281de6b5cc2ef0f0d11143
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-10-06 12:00:15 +0300

    Init game service repository

---
+++ src/main/java/com/epam/gameservice/GameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServiceApplication.class, args);
+	}
+
+}
