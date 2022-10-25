7e0ddd5a66f816340730627821978abb624b5af6
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-10-25 15:38:45 +0300

    remove discovery server client

--- src/main/java/com/epam/gameservice/GameServiceApplication.java
+++ src/main/java/com/epam/gameservice/GameServiceApplication.java
@@ -2,14 +2,12 @@ package com.epam.gameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class GameServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class, args);
+    }
 
 }
