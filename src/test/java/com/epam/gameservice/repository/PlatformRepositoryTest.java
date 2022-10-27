8dba9d337e31aa232962982655d3ad58d0080e59
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-10-27 18:06:41 +0300

    add tests for PlatformRepository

---
+++ src/test/java/com/epam/gameservice/repository/PlatformRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.epam.gameservice.repository;
+
+import com.epam.gameservice.entity.Platform;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Sql("classpath:sql/platforms.sql")
+class PlatformRepositoryTest {
+
+    @Autowired
+    private PlatformRepository platformRepository;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"nes", "NES"})
+    void shouldFindPlatformByCode(String code) {
+        Optional<Platform> nes = platformRepository.findByCode(code);
+
+        assertThat(nes)
+                .map(Platform::getCode)
+                .hasValue("NES");
+    }
+}
---
+++ src/test/resources/sql/platforms.sql
@@ -0,0 +1,5 @@
+INSERT INTO platforms(id, code, name, released_at)
+VALUES (1, 'NES', 'Nintendo Entertainment System', '1983-06-15');
+
+INSERT INTO platforms(id, code, name, released_at)
+VALUES (2, 'SMD', 'Sega Mega Drive', '1988-10-29');
