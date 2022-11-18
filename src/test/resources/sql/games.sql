c817ef9cb9ca8546979fa5cda14a6df2ab8fd41c
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-11-18 18:13:00 +0200

    add modified_at value to test sql scripts

--- src/test/resources/sql/games.sql
+++ src/test/resources/sql/games.sql
@@ -1,8 +1,8 @@
-INSERT INTO games(name, platform_id, released_at)
-VALUES ('Super Mario Bros.', 1, '1985-09-13');
+INSERT INTO games(name, platform_id, released_at, modified_at)
+VALUES ('Super Mario Bros.', 1, '1985-09-13', '2022-11-17');
 
-INSERT INTO games(name, platform_id, released_at)
-VALUES ('Battle City', 1, '1985-01-01');
+INSERT INTO games(name, platform_id, released_at, modified_at)
+VALUES ('Battle City', 1, '1985-01-01', '2022-11-17');
 
-INSERT INTO games(name, platform_id, released_at)
-VALUES ('Earthworm Jim', 2, '1994-01-01');
+INSERT INTO games(name, platform_id, released_at, modified_at)
+VALUES ('Earthworm Jim', 2, '1994-01-01', '2022-11-17');
--- src/test/resources/sql/platforms.sql
+++ src/test/resources/sql/platforms.sql
@@ -1,5 +1,5 @@
-INSERT INTO platforms(id, code, name, released_at)
-VALUES (1, 'NES', 'Nintendo Entertainment System', '1983-06-15');
+INSERT INTO platforms(id, code, name, released_at, modified_at)
+VALUES (1, 'NES', 'Nintendo Entertainment System', '1983-06-15', '2022-11-17');
 
-INSERT INTO platforms(id, code, name, released_at)
-VALUES (2, 'SMD', 'Sega Mega Drive', '1988-10-29');
+INSERT INTO platforms(id, code, name, released_at, modified_at)
+VALUES (2, 'SMD', 'Sega Mega Drive', '1988-10-29', '2022-11-17');
