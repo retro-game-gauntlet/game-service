904b2f5b5a83d7dba3ec9099ee453fbc0483a9e2
Oleksandr Diachenko <mail.positiv@gmail.com>
2022-11-28 13:11:12 +0200

    use static import

--- src/main/java/com/epam/gameservice/business/utils/formatter/MapWithoutBracketsStringFormatter.java
+++ src/main/java/com/epam/gameservice/business/utils/formatter/MapWithoutBracketsStringFormatter.java
@@ -3,9 +3,9 @@ package com.epam.gameservice.business.utils.formatter;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.lang.String.join;
+import static java.util.stream.Collectors.joining;
 
 @Component
 public class MapWithoutBracketsStringFormatter<K, V> implements StringFormatter<Map<K, V>> {
@@ -14,6 +14,6 @@ public class MapWithoutBracketsStringFormatter<K, V> implements StringFormatter<
     public String format(Map<K, V> map) {
         return map.entrySet().stream()
                 .map(entry -> join("=", entry.getKey().toString(), entry.getValue().toString()))
-                .collect(Collectors.joining(", "));
+                .collect(joining(", "));
     }
 }
